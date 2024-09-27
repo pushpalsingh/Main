5026cd56636afcdd504d070f8105ff0a2518059f
pushp <pushp@rdx>
2024-09-27 14:16:15 +0530

    third commit

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Main.main()");
 		System.out.println("Date "+ new Date());
+		System.out.println("Main.main()");
 	}
 
 }
