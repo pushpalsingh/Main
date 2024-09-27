d2feeaf02d580c21eb726854c5ee74222bf23536
pushp <pushp@rdx>
2024-09-27 14:14:44 +0530

    second commit

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+import java.util.Date;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Main.main()");
+		System.out.println("Date "+ new Date());
 	}
 
 }
